6db7cd78c696f0675b45a4cb368cbb7b25dd9b7f
Mitsu <developer.mitsu@gmail.com>
2019-05-13 18:10:31 +0900

    this is initial commit

---
+++ js/loading.js
@@ -0,0 +1,5 @@
+window.onload = function(){
+    document.getElementsByTagName("body")[0].setAttribute("style", "height: auto;")
+    document.getElementById("js-loading").setAttribute('style','display:none;');
+};
+
---
+++ js/main.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+    document.getElementById("js-nav-button").addEventListener('click', function(){
+        document.getElementById("js-nav-button").classList.toggle("active");
+        document.getElementById("js-nav").classList.toggle("active");
+    },false);
+
+    var elem = document.querySelector('.main-carousel');
+    var flkty = new Flickity( elem, {
+    // options
+    wrapAround: true,
+    cellAlign: 'left',
+    contain: true, 
+    autoPlay: 5000,
+    selectedAttraction: 0.01,
+    friction: 0.3,
+    });
+
+    
+
+    document.getElementById("js-watch").addEventListener("animationend", function(){
+        document.getElementById("js-animation").classList.add('none');
+    });
+
+});
