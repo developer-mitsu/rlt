d7366a85366c390cf130a823e43cd8acc6750afd
Mitsu <developer.mitsu@gmail.com>
2019-06-03 17:32:02 +0900

    [add]「」をアニメーションに追加

--- js/loading.js
+++ js/loading.js
@@ -1,5 +1,6 @@
 window.onload = function(){
     document.getElementsByTagName("body")[0].setAttribute("style", "height: auto;")
     document.getElementById("js-loading").setAttribute('style','display:none;');
+    
 };
 
