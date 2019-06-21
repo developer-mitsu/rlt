74271ea0ea2d50e6422369c9eeacb56736ef4ca2
Mitsu <developer.mitsu@gmail.com>
2019-06-21 15:37:16 +0900

    [fix]IE用にjs調整

--- js/main.js
+++ js/main.js
@@ -1,25 +1,46 @@
-document.addEventListener('DOMContentLoaded', function(){
+// js-nav-button").addEventListener('click', function(){
+//         document.getElementById("js-nav-button").classList.toggle("active");
+//         document.getElementById("js-nav").classList.toggle("active");
+//     },false);
 
-    document.getElementById("js-nav-button").addEventListener('click', function(){
-        document.getElementById("js-nav-button").classList.toggle("active");
-        document.getElementById("js-nav").classList.toggle("active");
-    },false);
-
-    var elem = document.querySelector('.main-carousel');
-    var flkty = new Flickity( elem, {
-    // options
-    wrapAround: true,
-    cellAlign: 'left',
-    contain: true, 
-    autoPlay: 5000,
-    selectedAttraction: 0.01,
-    friction: 0.3,
-    });
+//     var elem = document.querySelector('.main-carousel');
+//     var flkty = new Flickity( elem, {
+//     // options
+//     wrapAround: true,
+//     cellAlign: 'left',
+//     contain: true, 
+//     autoPlay: 5000,
+//     selectedAttraction: 0.01,
+//     friction: 0.3,
+//     });
 
     
 
-    document.getElementById("js-watch").addEventListener("animationend", function(){
-        document.getElementById("js-animation").classList.add('none');
-    });
+//     document.getElementById("js-watch").addEventListener("animationend", function(){
+//         document.getElementById("js-animation").classList.add('none');
+//     });
 
+// });
+
+
+"use strict";
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById("js-nav-button").addEventListener('click', function () {
+    document.getElementById("js-nav-button").classList.toggle("active");
+    document.getElementById("js-nav").classList.toggle("active");
+  }, false);
+  var elem = document.querySelector('.main-carousel');
+  var flkty = new Flickity(elem, {
+    // options
+    wrapAround: true,
+    cellAlign: 'left',
+    contain: true,
+    autoPlay: 5000,
+    selectedAttraction: 0.01,
+    friction: 0.3
+  });
+  document.getElementById("js-watch").addEventListener("animationend", function () {
+    document.getElementById("js-animation").classList.add('none');
+  });
 });
